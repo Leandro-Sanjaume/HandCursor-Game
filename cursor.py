47f269c5a9a8dac5103b2447755d5967b6cc2a42
Leandro Sanjaume <Alumno23.Sanjaume.Leandro@ipm.edu.ar>
2023-11-29 21:26:14 -0300

    Optimized and operational

--- cursor.py
+++ cursor.py
@@ -1,13 +1,16 @@
 import cv2
 import mediapipe as mp
 import pyautogui
+import time
 
 dragMode = True
+draw_landmarks = False
 cap = cv2.VideoCapture(0)
 hand_detector = mp.solutions.hands.Hands()
 drawing_utils = mp.solutions.drawing_utils
 screen_width, screen_height = pyautogui.size()
 index_y = 0
+
 while True:
     _, frame = cap.read()
     frame = cv2.flip(frame, 1)
@@ -18,31 +21,33 @@ while True:
 
     if hands:
         for hand in hands:
-            drawing_utils.draw_landmarks(frame, hand)
+            if draw_landmarks:
+                drawing_utils.draw_landmarks(frame, hand)
+
             landmarks = hand.landmark
             for id, landmark in enumerate(landmarks):
-                x = int(landmark.x*frame_width)
-                y = int(landmark.y*frame_height)
+                x = int(landmark.x * frame_width)
+                y = int(landmark.y * frame_height)
+
                 if id == 8:
-                    cv2.circle(img=frame, center=(x,y), radius=10, color=(0, 255, 255))
-                    index_x = screen_width/frame_width*x
-                    index_y = screen_height/frame_height*y
-                    pyautogui.moveTo()
+                    cv2.circle(img=frame, center=(x, y), radius=10, color=(0, 255, 255))
+                    index_x = screen_width / frame_width * x
+                    index_y = screen_height / frame_height * y
+                    if dragMode:
+                        pyautogui.dragTo(index_x, index_y)
+                    else:
+                        pyautogui.moveTo(index_x, index_y)
 
                 if id == 4:
-                    cv2.circle(img=frame, center=(x,y), radius=10, color=(0, 255, 255))
-                    thumb_x = screen_width/frame_width*x
-                    thumb_y = screen_height/frame_height*y
+                    cv2.circle(img=frame, center=(x, y), radius=10, color=(0, 255, 255))
+                    thumb_x = screen_width / frame_width * x
+                    thumb_y = screen_height / frame_height * y
 
-                    print('outside', abs(index_y - thumb_y))
-                    if abs(index_y - thumb_y) < 50 and dragMode == False:
+                    if abs(index_y - thumb_y) < 50 and not dragMode:
                         pyautogui.click()
-                        pyautogui.sleep(1)
-                    elif abs(index_y - thumb_y) < 150:
-                        if(dragMode):
-                            pyautogui.dragTo(index_x, index_y)
-                        else:
-                            pyautogui.moveTo(index_x, index_y) 
+                        pyautogui.sleep(0.5)
+
         print(pyautogui.position())
+
     cv2.waitKey(1)
     cv2.imshow('Mouse de mano', frame)
--- merged.py
+++ merged.py
@@ -192,13 +192,17 @@ def gameTest():
         game.new_game()
         game.game_over()
 
+dragMode = False
+draw_landmarks = False
 cap = cv2.VideoCapture(0)
 hand_detector = mp.solutions.hands.Hands()
 drawing_utils = mp.solutions.drawing_utils
 screen_width, screen_height = pyautogui.size()
 index_y = 0
+
 gameThread = threading.Thread(target=gameTest)
 gameThread.start()
+
 while True:
     _, frame = cap.read()
     frame = cv2.flip(frame, 1)
@@ -207,31 +211,36 @@ while True:
     output = hand_detector.process(rgb_frame)
     hands = output.multi_hand_landmarks
 
-
     if hands:
         for hand in hands:
-            drawing_utils.draw_landmarks(frame, hand)
+            if draw_landmarks:
+                drawing_utils.draw_landmarks(frame, hand)
+
             landmarks = hand.landmark
             for id, landmark in enumerate(landmarks):
-                x = int(landmark.x*frame_width)
-                y = int(landmark.y*frame_height)
+                x = int(landmark.x * frame_width)
+                y = int(landmark.y * frame_height)
+
                 if id == 8:
-                    cv2.circle(img=frame, center=(x,y), radius=10, color=(0, 255, 255))
-                    index_x = screen_width/frame_width*x
-                    index_y = screen_height/frame_height*y
-                    pyautogui.moveTo()
+                    cv2.circle(img=frame, center=(x, y), radius=10, color=(0, 255, 255))
+                    index_x = screen_width / frame_width * x
+                    index_y = screen_height / frame_height * y
+                    if dragMode:
+                        pyautogui.dragTo(index_x, index_y)
+                    else:
+                        pyautogui.moveTo(index_x, index_y)
+
 
                 if id == 4:
-                    cv2.circle(img=frame, center=(x,y), radius=10, color=(0, 255, 255))
-                    thumb_x = screen_width/frame_width*x
-                    thumb_y = screen_height/frame_height*y
+                    cv2.circle(img=frame, center=(x, y), radius=10, color=(0, 255, 255))
+                    thumb_x = screen_width / frame_width * x
+                    thumb_y = screen_height / frame_height * y
 
-                    print('outside', abs(index_y - thumb_y))
-                    if abs(index_y - thumb_y) < 40:
+                    if abs(index_y - thumb_y) < 50 and not dragMode:
                         pyautogui.click()
-                        pyautogui.sleep(0.2)
-                    elif abs(index_y - thumb_y) < 200:
-                        pyautogui.moveTo(index_x, index_y)
-        handPos = pyautogui.position()
+                        pyautogui.sleep(0.5)
+        print(pyautogui.position())
+
     cv2.waitKey(1)
+
 
